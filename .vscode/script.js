5008643567c26ea8b59e0b081fa3f6dbce7193ad
Rafael ferreira <rafael.ferreira20_@hotmail.com>
2024-02-14 10:22:26 -0300

    adicionado dark/light

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,8 +1,28 @@
-function toggleMode() {
-  const html = document.documentElement
+// function toggleMode() {
+//   const html = document.documentElement
 
-  if(html.classList.contains('light')) {
-    html.classList.remove('ligth')
-  } else{
-    html.classList.add('ligth')
-  }
+//   if(html.classList.contains('light'))
+//    {
+//     html.classList.remove('ligth')
+//   }
+//   else{
+//     html.classList.add('ligth')}
+//   }
+// function toggleMode() {
+//   const body = document.body
+//   body.classList.toggle("tema")
+// }
+
+    // function toggleTheme() {
+    //   const body = document.body;
+    //   body.classList.toggle('dark-theme');
+
+    //   const chk = document.getElementById('chk');
+    //   chk.checked = !chk.checked; // Inverte o estado do checkbox
+    // }
+
+  const chk = document.getElementById('chk')
+
+  chk.addEventListener('change', () => {
+    document.body.classList.toggle('tema')
+  })
